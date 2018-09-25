be29cb3899192c2c78406fc90ccdfa9a66d13a39
Oksana <oksanashukh@gmail.com>
2018-09-25 12:33:35 +0300

    adding text-message to progress bar manipulation

--- src/js/progressBarBy.js
+++ src/js/progressBarBy.js
@@ -2,8 +2,8 @@ var btn = document.querySelector('.add-val');
 btn.addEventListener('click', change_progress);
 function change_progress() {
         var elem = document.getElementById("myBar");
-        val = parseInt(document.getElementById("number").value);
-        var message = document.querySelector('.progress-mess'); 
+        var val = parseInt(document.getElementById("number").value);
+        var message = document.querySelector('.output'); 
         var width = 0;
         var id = setInterval(frame, val);
         function frame() {
@@ -13,7 +13,9 @@ function change_progress() {
                 width++; 
                 elem.style.width = width + '%';
                 elem.innerHTML = width * 1  + '%';
-                message.innerHTML = elem;
+
             }
+              message.innerHTML = val + '%';
         }  
+
     }
