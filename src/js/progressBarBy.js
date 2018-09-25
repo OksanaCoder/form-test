d577b879923982b6ee8eba13d5d0215ada31c316
Oksana <oksanashukh@gmail.com>
2018-09-25 13:03:33 +0300

    adding % to progress bar

--- src/js/progressBarBy.js
+++ src/js/progressBarBy.js
@@ -2,7 +2,7 @@ var btn = document.querySelector('.add-val');
 btn.addEventListener('click', change_progress);
 function change_progress() {
         var elem = document.getElementById("myBar");
-        var val = parseInt(document.getElementById("number").value);
+       var  val = parseInt(document.getElementById("number").value);
         var message = document.querySelector('.output'); 
         var width = 0;
         var id = setInterval(frame, val);
@@ -13,9 +13,9 @@ function change_progress() {
                 width++; 
                 elem.style.width = width + '%';
                 elem.innerHTML = width * 1  + '%';
-
+message.innerHTML = val + '%';
             }
-              message.innerHTML = val + '%';
+              
         }  
 
     }
