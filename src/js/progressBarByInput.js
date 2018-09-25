c3551b58ee8b56da75253f842b73ecb972c22954
Oksana <oksanashukh@gmail.com>
2018-09-25 20:38:09 +0300

    deleted progressBarBy script

--- src/js/progressBarByInput.js
+++ src/js/progressBarByInput.js
@@ -2,16 +2,23 @@ var btn = document.querySelector('.add-val');
 btn.addEventListener('click', change_progress);
 function change_progress() {
         var elem = document.getElementById("myBar");
-        val = parseInt(document.getElementById("number").value);
+       var  val = parseInt(document.getElementById("number").value);
+        var message = document.querySelector('.output'); 
         var width = 0;
         var id = setInterval(frame, val);
         function frame() {
             if (width >= val) {
                 clearInterval(id);
             } else {
+                console.log(message);
                 width++; 
-                elem.style.width = width + '%'; 
+                elem.style.width = width + '%';
                 elem.innerHTML = width * 1  + '%';
-            }
+                message.innerHTML = val + '%';
+                
+             }
+              
         }  
+
     }
+
